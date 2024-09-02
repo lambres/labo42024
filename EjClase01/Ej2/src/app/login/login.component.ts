8abcac3e8cbb4e35510c448679fc6b9e41cfc25e
lambres@gmail.com <dario@MacBook-Pro-de-Dario.local>
2024-09-02 13:55:15 -0300

    fin ej 2

--- EjClase01/Ej2/src/app/login/login.component.ts
+++ EjClase01/Ej2/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BienvenidoComponent } from "../bienvenido/bienvenido.component";
 import { ErrorComponent } from '../error/error.component';
+import { Usuario } from '../clases/usuario'
 
 @Component({
   selector: 'app-login',
@@ -14,8 +15,9 @@ export class LoginComponent {
   nombre: string = '';
   contrasenia: string = '';
   valido: boolean = false;
+  usuario: Usuario = new Usuario("dario","q1w2e3")
   login() {
-    if (this.nombre == 'dario' && this.contrasenia == 'q1w2e3') {
+    if (this.nombre == this.usuario.nombre && this.contrasenia == this.usuario.clave) {
       this.valido = true;
     } else {
       this.valido = false;
